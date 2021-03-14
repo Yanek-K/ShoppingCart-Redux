4659c12924479a56be2827b723ccbc59f0b36eaf
Yanek K <yanek.dev@gmail.com>
2021-03-13 22:02:52 -0500

    Add To Cart

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -1,16 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Product.module.css";
+import { connect } from "react-redux";
+import { addToCart } from "../../../redux/Shopping/shopping-actions";
 
-const Product = () => {
+const Product = ({ productData, addToCart }) => {
+  console.log(productData);
   return (
     <div className={styles.product}>
-      <img className={styles.product__image} src="" alt="" />
+      <img
+        className={styles.product__image}
+        src={productData.image}
+        alt={productData.title}
+      />
 
       <div className={styles.product__details}>
-        <p className={styles.details__title}>Title</p>
-        <p className={styles.details__desc}>Description</p>
-        <p className={styles.details__price}>$ 10.00</p>
+        <p className={styles.details__title}>{productData.title}</p>
+        <p className={styles.details__desc}>{productData.description}</p>
+        <p className={styles.details__price}>$ {productData.price}</p>
       </div>
 
       <div className={styles.product__buttons}>
@@ -19,7 +26,10 @@ const Product = () => {
             View Item
           </button>
         </Link>
-        <button className={`${styles.buttons__btn} ${styles.buttons__add}`}>
+        <button
+          onClick={() => addToCart(productData.id)}
+          className={`${styles.buttons__btn} ${styles.buttons__add}`}
+        >
           Add To Cart
         </button>
       </div>
@@ -27,4 +37,10 @@ const Product = () => {
   );
 };
 
-export default Product;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addToCart: (id) => dispatch(addToCart(id)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(Product);
